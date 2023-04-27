3fae685f0696dc0435b1d8f841c4d9982ec2f378
Nidheeshkrishna <nkrishna005@gmail.com>
2023-04-27 15:31:40 +0530

    platform channel

---
+++ android/app/src/main/java/com/example/platform_channel_app/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.platform_channel_app;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.plugin.common.EventChannel.StreamHandler;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+public class MainActivity extends FlutterActivity {
+    private static final String BATTERY_CHANNEL = "samples.flutter.io/battery";
+    private static final String CHARGING_CHANNEL = "samples.flutter.io/charging";
+    private BroadcastReceiver chargingStateChangeReceiver;
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+
+
+        new EventChannel(flutterEngine.getDartExecutor(), CHARGING_CHANNEL).setStreamHandler(
+              new StreamHandler() {
+
+
+
+
+
+
+                    @Override
+                    public void onListen(Object arguments, EventChannel.EventSink events) {
+                        chargingStateChangeReceiver = createChargingStateChangeReceiver(events);
+                        registerReceiver(
+                                chargingStateChangeReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+                    }
+
+                    public void onCancel(Object arguments) {
+                        unregisterReceiver(chargingStateChangeReceiver);
+                        chargingStateChangeReceiver = null;
+                    }
+                }
+        );
+        new MethodChannel(flutterEngine.getDartExecutor(), BATTERY_CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                        if (call.method.equals("getBatteryLevel")) {
+                            int batteryLevel = getBatteryLevel();
+
+                            if (batteryLevel != -1) {
+                                result.success(batteryLevel);
+                            } else {
+                                result.error("UNAVAILABLE", "Battery level not available.", null);
+                            }
+                        } else {
+                            result.notImplemented();
+                        }
+                    }
+                }
+        );
+    }
+
+
+
+    private BroadcastReceiver createChargingStateChangeReceiver(final EventChannel.EventSink events) {
+        return new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+
+                if (status == BatteryManager.BATTERY_STATUS_UNKNOWN) {
+                    events.error("UNAVAILABLE", "Charging status unavailable", null);
+                } else {
+                    boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                            status == BatteryManager.BATTERY_STATUS_FULL;
+                    events.success(isCharging ? "charging" : "discharging");
+                }
+            }
+        };
+
+    }
+
+    private int getBatteryLevel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            return batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            return (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+    }
+}
