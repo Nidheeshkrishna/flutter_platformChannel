2abf56e03a256144c7e3f3881cb2c497a7139da8
Nidheeshkrishna <nkrishna005@gmail.com>
2023-04-27 17:47:50 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/example/platform_channel_app/MainActivity.java
+++ android/app/src/main/java/com/example/platform_channel_app/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.platform_channel_app;
 
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.ContextWrapper;
@@ -18,21 +17,17 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+
 public class MainActivity extends FlutterActivity {
     private static final String BATTERY_CHANNEL = "samples.flutter.io/battery";
     private static final String CHARGING_CHANNEL = "samples.flutter.io/charging";
     private BroadcastReceiver chargingStateChangeReceiver;
+
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
 
-
         new EventChannel(flutterEngine.getDartExecutor(), CHARGING_CHANNEL).setStreamHandler(
-              new StreamHandler() {
-
-
-
-
-
+                new StreamHandler() {
 
                     @Override
                     public void onListen(Object arguments, EventChannel.EventSink events) {
@@ -45,8 +40,7 @@ public class MainActivity extends FlutterActivity {
                         unregisterReceiver(chargingStateChangeReceiver);
                         chargingStateChangeReceiver = null;
                     }
-                }
-        );
+                });
         new MethodChannel(flutterEngine.getDartExecutor(), BATTERY_CHANNEL).setMethodCallHandler(
                 new MethodChannel.MethodCallHandler() {
                     @Override
@@ -63,11 +57,8 @@ public class MainActivity extends FlutterActivity {
                             result.notImplemented();
                         }
                     }
-                }
-        );
+                });
     }
-
-
 
     private BroadcastReceiver createChargingStateChangeReceiver(final EventChannel.EventSink events) {
         return new BroadcastReceiver() {
@@ -92,8 +83,8 @@ public class MainActivity extends FlutterActivity {
             BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
             return batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
         } else {
-            Intent intent = new ContextWrapper(getApplicationContext()).
-                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            Intent intent = new ContextWrapper(getApplicationContext()).registerReceiver(null,
+                    new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
             return (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
                     intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         }
